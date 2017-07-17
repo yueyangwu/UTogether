0737e1f7b94c84ba8f9df286762f4a9c779d4be4
WuYueyang <YueyangWu@WuYueyangs-MacBook-Pro.local>
2017-07-17 20:49:40 +0800

    database created

--- app_server/controllers/events.js
+++ app_server/controllers/events.js
@@ -1,15 +1,38 @@
 /* GET 'home' page */
 module.exports.eventsList = function(req, res){
-res.render('eventslist', { title: 'Home' });
+res.render('eventslist', { 
+    title: 'Event List',
+    events: [{
+        title: 'Pumpkin Carving Night',
+        date: '10/23/2017',
+        time: '17:30',
+        joined: '18 joined',
+        distance: '100m'
+    },
+    {
+        title: 'Introduction to Authentic Chinese Food',
+        date: '10/13/2017',
+        time: '13:10',
+        joined: '29 joined',
+        distance: '200m'
+    },
+    {
+        title: 'International Faculty & Scholars Social Hour',
+        date: '10/9/2017',
+        time: '10:00',
+        joined: '10 joined',
+        distance: '250m'
+    }]
+ });
 };
 /* GET 'Event info' page */
 module.exports.eventInfo = function(req, res){
-res.render('eventinfo', { title: 'Location info' });
+res.render('eventinfo', { title: 'Location Info' });
 };
 /* GET 'Add event' page */
 module.exports.addEvent = function(req, res){
-res.render('addevent', { title: 'Add event' });
+res.render('addevent', { title: 'Add Event' });
 };
 module.exports.myEvent = function(req, res){
-res.render('index', { title: 'My events' });
+res.render('index', { title: 'My Events' });
 };
---
+++ app_server/models/db.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql')
+var con = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'password',
+  database : 'UTogether'
+});
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+  if(err) console.log('err: ', err);
+  else console.log('Terminated done: ');
+});
