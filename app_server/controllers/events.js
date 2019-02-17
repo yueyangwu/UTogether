a34953162a16be6bd96e055f2454d2dae65304e2
WuYueyang <YueyangWu@WuYueyangs-MBP.attlocal.net>
2019-02-17 17:57:58 -0600

    wip aaa

--- app_server/controllers/events.js
+++ app_server/controllers/events.js
@@ -3,7 +3,7 @@ var apiOptions = {
     server : "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = "https://utogether.herokuapp.com/";
+    apiOptions.server = "https://utogether.herokuapp.com";
 }
 
 /* GET 'home' page */
